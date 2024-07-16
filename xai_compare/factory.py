49fb34c157012453d1df757956e04e5a5a0b5996
anidima <da.gufranov@gmail.com>
2024-07-17 02:14:20 +0300

    "updated comparison-factory, main, permutations"

--- xai_compare/factory.py
+++ xai_compare/factory.py
@@ -1,4 +1,97 @@
 # ----------------------------------------------------------------------------------------------------
+# Class ExplainerFactory
+# This class is used to create Explainer objects based on what word is entered into the create() function
+#
+# ------------------------------------------------------------------------------------------------------
+
+import string
+from typing import Any
+
+import pandas as pd
+
+# Local application imports
+from xai_compare.explainer import Explainer
+from xai_compare.explainers.lime_wrapper import LIME
+from xai_compare.explainers.shap_wrapper import SHAP
+from xai_compare.explainers.permutation_wrapper import Permutations
+from xai_compare.config import MODE, EXPLAINERS
+
+
+class ExplainerFactory:
+    """
+    A class for creating classes or instances of various explainer types based on the given model and data.
+
+    This class simplifies the process of instantiating different types of explainers by providing a common interface
+    to specify the necessary training and testing datasets along with the model.
+
+    Attributes:
+        model (Any): The machine learning model to be explained.
+        X_train (pd.DataFrame): Training features dataset.
+        X_test (pd.DataFrame): Testing features dataset.
+        y_train (pd.DataFrame): Training target dataset.
+        y_test (pd.DataFrame): Testing target dataset.
+        mode (str): Mode of operation, specifies if the model is for 'regression' or 'classification'.
+    """
+    def __init__(self,
+                 model: Any = None,
+                 X_train: pd.DataFrame = None,
+                 X_test: pd.DataFrame = None,
+                 y_train: pd.DataFrame = None,
+                 y_test: pd.DataFrame = None,
+                 mode: str = MODE.REGRESSION):
+        self.model = model
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+        self.mode = mode
+
+
+    def create_explainer(self, explainer_type: string) -> Explainer:
+        """
+        Creates and returns an explainer object based on the specified type.
+
+        This method allows for dynamic creation of different types of explainer objects based on a predefined string
+        identifier. If the model is not set, it returns the class of the explainer for manual instantiation.
+
+        Parameters:
+            explainer_type (str): A string identifier for the explainer type. Valid options are "shap", "lime", "permutations".
+
+        Returns:
+            Explainer: An instance of the requested explainer type with the provided model and data.
+
+        Raises:
+            ValueError: If the explainer_type is not recognized or unsupported.
+        """
+        if explainer_type == "shap":
+            if self.model:
+                shapEx = SHAP(self.model, self.X_train, self.y_train)
+            else:
+                shapEx = SHAP
+            return shapEx
+        elif explainer_type == "lime":
+            if self.model:
+                limeEx = LIME(self.model, self.X_train, self.y_train)
+            else:
+                limeEx = LIME
+            return limeEx
+        elif explainer_type == "permutations":
+            if self.model:
+                permEx = Permutations(self.model, self.X_train, self.y_train)
+            else:
+                permEx = Permutations
+            return permEx
+
+
+        # If there are more explainers you want to account for, the code can be added here:
+
+        else:
+            # Raise an exception for an invalid explainer type
+            raise ValueError("Invalid Explainer type provided")
+
+
+
+# ----------------------------------------------------------------------------------------------------
 # Class ComparisonFactory
 # This class is used to create Comparison objects based on the name of comparison technique and explainers
 #
@@ -104,9 +197,8 @@ class ComparisonFactory:
             return consistencyTchn
 
 
-
-        # If there are more explainers you want to account for, the code can be added here:
+        # If there are more Comparison Techniques you want to account for, the code can be added here:
 
         else:
-            # throw exception
-            print("invalid Comparison Technique")
+            # Raise an exception for an invalid Comparison Technique
+            raise ValueError("Invalid Comparison Technique provided")
