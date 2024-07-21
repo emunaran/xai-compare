a76e1f4cd83356127185a2504de6d3681ddf2edc
anidima <da.gufranov@gmail.com>
2024-07-21 09:44:32 +0300

    wrappers docstrings

--- xai_compare/explainers/lime_wrapper.py
+++ xai_compare/explainers/lime_wrapper.py
@@ -17,10 +17,26 @@ warnings.filterwarnings("ignore", category=UserWarning, message=".*X does not ha
 
 
 class LIME(Explainer):
+    """
+    A class that encapsulates the LIME (Local Interpretable Model-agnostic Explanations) method for explaining model predictions.
+
+    Attributes:
+        model: An input machine learning model.
+        mode (str): Indicates whether the explainer is used for 'regression' or 'classification'.
+    """
 
     __name__ = "LIME"
 
     def explain_global(self, X_data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Provides a global explanation of the model by averaging the local explanations across all instances.
+
+        Parameters:
+            X_data (pd.DataFrame): The dataset for which global explanations are generated.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing global importance scores for each feature.
+        """
         local_exps = self.explain_local(X_data)
         # Calculate the mean across rows to get the average effect of each feature globally
         global_exp = np.mean(local_exps, axis=0)
@@ -29,6 +45,15 @@ class LIME(Explainer):
 
 
     def explain_local(self, X_data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Provides local explanations for each instance in the dataset, explaining the contribution of each feature to the individual prediction.
+
+        Parameters:
+            X_data (pd.DataFrame): The dataset for which local explanations are generated.
+
+        Returns:
+            pd.DataFrame: A DataFrame where each row represents an instance with feature contributions for that specific prediction.
+        """
         # Initialize the LIME explainer for tabular data
         explainer = lime.lime_tabular.LimeTabularExplainer(
             training_data=X_data.values,
